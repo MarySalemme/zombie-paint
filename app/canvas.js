0dfd769ae479a0faa438f0abfc6db146a20de080
MarySalemme <marysalemme1@gmail.com>
2017-08-14 16:22:50 +0100

    Setting up mocha to run in browser

---
+++ app/canvas.js
@@ -0,0 +1,7 @@
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+
+(function () {
+  context.lineTo(100, 150);
+  context.stroke();
+})();
---
+++ test/canvas.js
@@ -0,0 +1,7 @@
+var expect = chai.expect;
+
+describe('whatever', function() {
+  it("does something", function () {
+    expect(1).to.equal(1);
+  });
+});
