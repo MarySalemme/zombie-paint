a856e050a939a8c385d1f602308c2d1a98d20613
Miho Tse <mihotse@gmail.com>
2017-08-16 11:03:19 +0100

    Add drawing and create dot methods

--- public/app/src/canvas.js
+++ public/app/src/canvas.js
@@ -3,11 +3,27 @@ function Canvas(element) {
   this.height = 100;
   this.width = 200;
   this.ctx = this.element.getContext('2d');
-  this.isDrawing = false;
+  this._drawing = false;
   this.xAxis = [];
   this.yAxis = [];
 }
 
+Canvas.prototype.isDrawing = function () {
+  return this._drawing
+};
+
+Canvas.prototype.startDrawing = function () {
+  this._drawing = true;
+};
+
+Canvas.prototype.endDrawing = function () {
+  this._drawing = false;
+};
+
+Canvas.prototype.createDot = function (x, y) {
+  this.xAxis.push(x);
+  this.yAxis.push(y);
+};
 // Canvas.prototype.drawRect = function() {
 //   this.ctx.fillRect(10, 10, 100, 200)
 // }
--- test/canvas.js
+++ test/canvas.js
@@ -9,41 +9,65 @@ describe ('Canvas', function() {
 
   describe ('element', function() {
     it('contains canvas element', function() {
-      expect(customCanvas.element).to.equal(canvas)
-    })
-  })
+      expect(customCanvas.element).to.equal(canvas);
+    });
+  });
 
   describe ('height', function() {
     it('has a height', function() {
-      expect(customCanvas.height).to.equal(100)
-    })
-  })
+      expect(customCanvas.height).to.equal(100);
+    });
+  });
 
   describe ('width', function() {
     it('has a width', function() {
-      expect(customCanvas.width).to.equal(200)
-    })
-  })
-
-  describe('isDrawing', function() {
-    it("has an initial value of false", function () {
-      expect(customCanvas.isDrawing).to.equal(false)
+      expect(customCanvas.width).to.equal(200);
     });
   });
 
   describe('xAxis', function() {
     it("starts as an empty array", function () {
-      expect(customCanvas.xAxis).to.be.empty
+      expect(customCanvas.xAxis).to.be.empty;
     });
   });
 
   describe('yAxis', function() {
     it("starts as an empty array", function () {
-      expect(customCanvas.yAxis).to.be.empty
+      expect(customCanvas.yAxis).to.be.empty;
     });
   });
 
-  
+  describe('#isDrawing', function() {
+    it("returns a boolean value", function() {
+      expect(customCanvas.isDrawing()).to.equal(false);
+    });
+  });
+
+  describe('#startDrawing', function() {
+    it("changes the value of thhe drawing attribute to be true", function () {
+      customCanvas.startDrawing();
+      expect(customCanvas.isDrawing()).to.equal(true);
+    });
+  });
+
+  describe('#endDrawing', function() {
+    it("changes the value of thhe drawing attribute to be false", function () {
+      customCanvas.endDrawing();
+      expect(customCanvas.isDrawing()).to.equal(false);
+    });
+  });
+
+  describe('#createDot', function() {
+    it("creates an x-coordinate", function() {
+      customCanvas.createDot(10, 50);
+      expect(customCanvas.xAxis[0]).to.equal(10);
+    });
+
+    it("creates a y-coordinate", function() {
+      customCanvas.createDot(10, 50);
+      expect(customCanvas.yAxis[0]).to.equal(50);
+    });
+  });
   // describe ('drawRect', function() {
   //   customCanvas.drawRect();
   //   stack = JSON.parse(ctx.json());
