b77dce37ab08d0dc86afe267de932df33987de39
MarySalemme <marysalemme1@gmail.com>
2017-08-17 11:17:32 +0100

    First implementation of stroke model

---
+++ public/app/src/stroke.js
@@ -0,0 +1,15 @@
+function Stroke() {
+  this.size = 10;
+  this.currentColour = '#000';
+  this.red = '#ff0000';
+  this.blue = '#0000ff';
+  this.green = '#008000';
+};
+
+Stroke.prototype.changeSize = function(size) {
+  this.size = size;
+};
+
+Stroke.prototype.changeColour = function(colour) {
+  this.currentColour = colour;
+};
