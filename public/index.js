c14172c6adac6407b1a099e071bebec285deb18e
Jasper King-Harman <j.kingharman@googlemail.com>
2017-08-18 16:01:11 +0100

    Tidying up tests and app code

--- public/index.js
+++ public/index.js
@@ -15,24 +15,10 @@ function setUpController(controller = new Controller(buildCanvas())) {
   return controller = controller;
 }
 
-//will this method be changed or removed at some point?
-// function setCanvasSize(document, $) {
-//   htmlCanvas = document.getElementById('canvas')
-//   $('#canvas').attr({
-//     width: window.innerWidth,
-//     height:window.innerHeight
-//   });
-// }
-
 function pageElementSetup(document) {
   getCanvasElement(document)
   buildCanvas(document)
   setUpController(document);
-  // setCanvasSize(document, $);
 }
 
 onDomReady(window, pageElementSetup(document))
-
-
-
-
--- test/setUpFunctions.js
+++ test/setUpFunctions.js
@@ -7,10 +7,8 @@ var expect = chai.expect;
     mockWindow = { onload: null };
     mockCanvasElement = 'canvas'
     mockDocument = { getElementById: () => mockCanvasElement};
-    mockJquery = {};
     mockCanvasModel = "the canvas"
     mockControllerModel = "the model"
-    mock$ = { attr: () => chai.spy() };
 
     describe('#onDomReady', function() {
         it("loads passed function on load", function() {
@@ -27,7 +25,6 @@ var expect = chai.expect;
         })
     })
 
-    //is this too basic?
     describe('#buildCanvas', function() {
         it('sets up a new canvas', function() {
             expect(buildCanvas(mockCanvasModel)).to.equal(mockCanvasModel)
@@ -38,19 +35,6 @@ var expect = chai.expect;
         it('instantiates a new controller with a canvas', function() {
             expect(setUpController(mockControllerModel)).to.equal(mockControllerModel)
         })
-    })
-
-    describe('#setCanvasSize', function() {
-        it('calls get element by id on document', function() {
-            let spy = chai.spy.on(mockDocument, 'getElementById');
-            getCanvasElement(mockDocument)
-            expect(spy).to.have.been.called()
-        })
-
-        // it('adds a height and width to the canvas', function() {
-        //     setCanvasSize(mockDocument, mock$)
-        //     expect(mock$.attr).to.have.been.called 
-        // })
     })
 
     describe('#pageElementSetup', function() {
